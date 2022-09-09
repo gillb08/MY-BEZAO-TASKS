041a3e7729f08057d305a87e38980b204caf7531
Victor Gilbert Chijioke <111375035+gillb08@users.noreply.github.com>
2022-09-09 14:22:06 +0100

    Add files via upload

---
+++ Code_Cracker.js
@@ -0,0 +1,11 @@
+function encrypt(... param) {
+    var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var encry = ['£', '*', '%', '&', '<', '>', '!', ')', '"', '(', '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'];
+
+    let output = "";
+
+    let index = alpha.indexOf("g", "h");
+    output += encry[index];
+    return output;
+}
+console.log(encrypt()); 

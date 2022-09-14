f5caeb3c4c0631ff6e995827a83414bee356d3a0
Victor Gilbert Chijioke <111375035+gillb08@users.noreply.github.com>
2022-09-14 04:27:54 +0100

    Update Code_Cracker.js

--- Code_Cracker.js
+++ Code_Cracker.js
@@ -2,10 +2,32 @@ function encrypt() {
     var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     var encry = ['£', '*', '%', '&', '<', '>', '!', ')', '"', '(', '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'];
 
-    let output = "";
+     function encrypt (message, type) {
+            let char = message.split("");
+            let output = [];
+            if (type == "decrypt") {
+                for (let index = 0; index < char.length; index++) {
+                    let findIndx = encry.indexOf(char[index]);
+                    if (findIndx != null || undefined) {
+                        output.push(alpha[findIndx]);
+                    } else {
+                        ; eer
+                        continue;
+                    }
+                }
+            }
+            if (type === "encrypt") {
+                for (let index = 0; index < char.length; index++) {
+                    let findIndx = alpha.indexOf(char[index]);
+                    if (findIndx != null || undefined) {
+                        output.push(encry[findIndx]);
+                    } else {
+                        continue;
+                    }
+                }
+            }
 
-    let index = alpha.indexOf("g", "h");
-    output += encry[index];
-    return output;
+            return output.join("");
+        };
 }
-console.log(encrypt()); 
+console.log(encrypt(" chiijioke", "decrypt"));
